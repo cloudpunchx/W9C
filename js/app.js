75445be4a975ad88d3ca55271cfee6929e156d67
cloudpunchx <siobhanbonardi@hotmail.com>
2022-11-03 20:35:56 -0600

    Created some array variables and variables and assigned values.

--- js/app.js
+++ js/app.js
@@ -2,3 +2,25 @@ let itemNames = [`journal`, `candle`, `markers`];
 let itemPrices = [`$22.50`, `$39.99`, `$16`];
 let inStock = [true, true, false];
 
+let firstItemName = itemNames[0];
+console.log(firstItemName);
+
+let firstItemPrice = itemPrices[0];
+console.log(firstItemPrice);
+
+itemNames.push(`mug`);
+itemPrices.push(`$18.99`);
+inStock.push(true);
+
+// I was just checking to make sure my PUSH worked (then commented out (obviously))
+// let lastItemPrice = itemPrices[3];
+// console.log(lastItemPrice);
+
+let lastItemName=itemNames.pop();
+let lastItemPrice=itemPrices.pop();
+let lastItemStock=inStock.pop();
+
+// testing to make sure my POP functions worked - they did!!
+console.log(lastItemName);
+console.log(lastItemPrice);
+console.log(lastItemStock);
